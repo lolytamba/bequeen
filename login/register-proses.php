4ef5c8a0fc3f6c206d96a0e7808fc6740c1b1abf
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-18 09:01:50 +0700

    move to login

--- login/register-proses.php
+++ login/register-proses.php
@@ -2,11 +2,11 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/phpmailer/phpmailer/src/Exception.php';
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 	if(isset($_POST['register']))
 	{

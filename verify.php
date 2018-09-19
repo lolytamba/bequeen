92a00d0002794eff1694c808755728ac395448a6
lolytamba <lolyeliatamba@gmail.com>
2018-09-19 09:49:22 +0700

    active

--- verify.php
+++ verify.php
@@ -5,10 +5,9 @@
     $email = $_GET['email'];
     $active = $_GET['active'];
 
-    //if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['active']) && !empty($_GET['active'])){
-        if(isset($_GET['active']) && !empty($_GET['active'])){
-       // $email = mysqli_escape_string($con, $email);
-       // $active = mysqli_escape_string($con, $_POST['active']);    
+    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['active']) && !empty($_GET['active'])){
+        $email = mysqli_escape_string($con, $email);
+        $active = mysqli_escape_string($con, $active);    
 
         $search = mysqli_query($con, "SELECT email, active, status FROM register WHERE email='".$email."' AND active='".$active."' AND status='0'") or die(mysqli_sconnect_error());
         $match = mysqli_num_rows($search);

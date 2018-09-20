c2e81b5627545f99b30610e62e716799cbfd2960
lolytamba <lolyeliatamba@gmail.com>
2018-09-20 16:41:54 +0700

    verify_3

--- verify.php
+++ verify.php
@@ -10,8 +10,8 @@
 
     if($row['status']=='0'){
         mysqli_query($con, "UPDATE register SET status='1' WHERE email='".$email."' AND active='".$active."' ") or die(mysqli_connect_error());
-        echo '<div class="statusMsg">Your account has be actived, you can now login</div>';
+        echo 'Your account has be actived, you can now login!';
     }else{
-        echo '<div class="statusmsg">The url is either invalid or you already have activated your account.</div>';
+        echo 'The url is either invalid or you already have activated your account.';
     }
 ?>

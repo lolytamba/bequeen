771ea74faf9d3af984d81638cd6d47ad9274601d
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-21 19:20:33 +0700

    update

--- profile/update.php
+++ profile/update.php
@@ -8,7 +8,7 @@
         $password       =$_POST['password'];
         $password_hash  = password_hash($password, PASSWORD_DEFAULT);
 
-        $update = mysqli_query($con, "UPDATE register SET nama='$nama', no_hp='$phone', password='$password_hash'  WHERE email='$email' ") or die(mysqli_error($con));
+        $update = mysqli_query($con, "UPDATE register SET nama='$nama', no_hp = '$phone', password = '$password_hash' where email = '$email' ");
         if($update){
             echo 'Profile updated';
             header('location : profile.php');

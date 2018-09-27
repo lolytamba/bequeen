ed99a431fa2abca2b3b7f8d6ad723f1490f00e96
frentzenzero <frentzen.zero@gmail.com>
2018-09-27 22:27:49 +0700

    foto

--- profile/profile.php
+++ profile/profile.php
@@ -27,12 +27,13 @@
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-blue w3-card" id="myNavbar">
-    <a href="profile.php"class="w3-bar-item w3-button w3-wide">Hi, <?php echo $nama;?>!</a>
+  <a href="profile.php"class="w3-bar-item w3-button w3-wide">Hi, <?php echo $nama;?>!</a>
     <!-- Right-sided navbar links -->
     <div class="w3-right w3-hide-small">
       <a href="../index.php" class="w3-bar-item w3-button">HOME</a>
       <a href="../Services.php" class="w3-bar-item w3-button"><i class="fa fa-th"></i> SERVICES</a>
-      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+      <a href="../Booking/booking.php" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> BOOK</a>
+      <a href="../index.php#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
       <a href="../login/logout.php" class="w3-bar-item w3-button"><i class="fa fa-user"></i>LOGOUT</a>
     </div>
     <!-- Hide right-floated links on small screens and replace them with a menu icon -->
@@ -42,17 +43,14 @@
     </a>
   </div>
 </div>
+    <!-- Hide right-floated links on small screens and replace them with a menu icon -->
 
-<!-- Sidebar on small screens when clicking the menu icon -->
-<nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
-  <a href="#home" onclick="w3_close()" class="w3-bar-item w3-button">HOME</a>
-  
-  <a href="#work" onclick="w3_close()" class="w3-bar-item w3-button">SERVICES</a>
-  
-  <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
-  <a href="#login" onclick="w3_close()" class="w3-bar-item w3-button">LOGIN</a>
-</nav>
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
+</div>
+
 
 <!-- Header with full-height image -->
 <header class="bgimg-1 w3-display-container" id="home"></header>

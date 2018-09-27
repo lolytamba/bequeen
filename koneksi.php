b678f26227e56863b6157968d0f46aae1c8fddf2
lolytamba <lolyeliatamba@gmail.com>
2018-09-27 18:42:17 +0700

    koneksi fix

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,13 @@
 <?php
-    // $server="localhost";
-    // $username="thekingc";
-    // $password="Zn@44N#GxuWw73";
-    // $database="thekingc_bequeen";
-
     $server="localhost";
-    $username="root";
-    $password="";
+    $username="thekingc";
+    $password="Zn@44N#GxuWw73";
     $database="thekingc_bequeen";
+
+    // $server="localhost";
+    // $username="root";
+    // $password="";
+    // $database="thekingc_bequeen";
 
   $con = mysqli_connect($server,$username,$password,$database) or die(mysqli_connect_error());
 ?>
